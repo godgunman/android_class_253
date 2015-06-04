35ed87c31da55034fe9833b17b649fab2dc4e33a
ggm <godgunman@gmail.com>
2015-06-04 19:31:15 +0800

    use findViewById to get instance

--- SimpleUI/app/src/main/java/com/example/simpleui/MainActivity.java
+++ SimpleUI/app/src/main/java/com/example/simpleui/MainActivity.java
@@ -4,14 +4,23 @@ import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
-
+import android.widget.Button;
+import android.widget.EditText;
 
 public class MainActivity extends ActionBarActivity {
+
+    private EditText inputEditText;
+    private Button sendButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        inputEditText = (EditText) findViewById(R.id.editText);
+        sendButton = (Button) findViewById(R.id.button);
+
+        sendButton.setText("renamed");
     }
 
     @Override
