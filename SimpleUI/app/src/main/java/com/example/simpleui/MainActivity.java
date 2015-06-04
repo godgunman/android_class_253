c0537ac4aa65c271205dd6ff24f0542abc920efd
ggm <godgunman@gmail.com>
2015-06-04 19:57:34 +0800

    implement setOnClickListener

--- SimpleUI/app/src/main/java/com/example/simpleui/MainActivity.java
+++ SimpleUI/app/src/main/java/com/example/simpleui/MainActivity.java
@@ -4,8 +4,10 @@ import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.Toast;
 
 public class MainActivity extends ActionBarActivity {
 
@@ -20,7 +22,19 @@ public class MainActivity extends ActionBarActivity {
         inputEditText = (EditText) findViewById(R.id.editText);
         sendButton = (Button) findViewById(R.id.button);
 
-        sendButton.setText("renamed");
+        sendButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                send();
+            }
+        });
+
+    }
+
+    private void send() {
+
+        String text = inputEditText.getText().toString();
+        Toast.makeText(this, text, Toast.LENGTH_SHORT).show();
     }
 
     @Override
